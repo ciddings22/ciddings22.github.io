801d9eb057d9973f679319967c8d74c375a9432f
Iddings Caleb <824881@stma.k12.mn.us>
2022-04-11 11:43:22 -0500

    Made password page

--- secret.js
+++ secret.js
@@ -1,3 +1,10 @@
-if (document.getElementById("password").value == "bruh") {
-	
+function submit() {
+	if (document.getElementById("password").value == "bruh") {
+	document.getElementById("output").innerHTML =  "ACCESS GRANTED";
+	}
+	else {
+		document.getElementById("output").innerHTML =  "PLEASE ENTER CORRECT PASSWORD";
+	}
 }
+
+document.getElementById("submit").addEventListener("click", submit);
